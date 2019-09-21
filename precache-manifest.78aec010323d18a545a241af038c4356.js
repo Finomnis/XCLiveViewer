0dc518ba4103e5b9a02105a7810c45698c774c43
circleci-build <build@circleci.com>
2019-09-21 11:08:09 +0000

    [skip ci] Updates

---
+++ precache-manifest.78aec010323d18a545a241af038c4356.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66ae1db1c0591bd2f892d63f1e53a94",
+    "url": "/XCLiveViewer/index.html"
+  },
+  {
+    "revision": "a4f6b0ba79e8248b8ff0",
+    "url": "/XCLiveViewer/static/css/main.88e49e7f.chunk.css"
+  },
+  {
+    "revision": "e7ee6b47da8376eb5ebe",
+    "url": "/XCLiveViewer/static/js/2.afe74032.chunk.js"
+  },
+  {
+    "revision": "a4f6b0ba79e8248b8ff0",
+    "url": "/XCLiveViewer/static/js/main.fa7b9929.chunk.js"
+  },
+  {
+    "revision": "245f697e905c15a39b5e",
+    "url": "/XCLiveViewer/static/js/runtime-main.1c143e6b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/XCLiveViewer/precache-manifest.a4e7b785cd4856b0c3eaf9f0227427f1.js"
+  "/XCLiveViewer/precache-manifest.78aec010323d18a545a241af038c4356.js"
 );
 
 self.addEventListener('message', (event) => {
