090bcf69d921e7bcf14128449c0a9af670189905
Finomnis <finomnis@gmail.com>
2019-09-26 14:08:41 +0200

    Added status popover to title bar

--- src/pages/TitleBar.jsx
+++ src/pages/TitleBar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useState } from "react";
+
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
+import Popover from "@material-ui/core/Popover";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Badge from "@material-ui/core/Badge";
@@ -20,6 +23,10 @@ import { CircularProgress } from "@material-ui/core";
 const useStyles = makeStyles(theme => ({
   menuButton: {
     marginRight: theme.spacing(2)
+  },
+  popover: {
+    padding: theme.spacing(1),
+    align: "right"
   }
 }));
 
@@ -48,6 +55,8 @@ const LoadingBadge = props => {
 const TitleBar = () => {
   const classes = useStyles();
   const connectionState = useXContestConnectionState();
+
+  const [connectionPopAnchor, setConnectionPopAnchor] = useState(null);
 
   const renderConnectionIcon = () => {
     switch (connectionState) {
@@ -95,15 +104,31 @@ const TitleBar = () => {
           </Box>
 
           <IconButton
-            disableRipple
-            disableFocusRipple
             edge="end"
             color="inherit"
+            onClick={event => setConnectionPopAnchor(event.currentTarget)}
           >
             {renderConnectionIcon()}
           </IconButton>
         </Toolbar>
       </AppBar>
+      <Popover
+        open={Boolean(connectionPopAnchor)}
+        anchorEl={connectionPopAnchor}
+        onClose={() => setConnectionPopAnchor(null)}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right"
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right"
+        }}
+      >
+        <Box className={classes.popover}>
+          <Typography>{connectionState}</Typography>
+        </Box>
+      </Popover>
     </div>
   );
 };
