36b146709fbcdf5818fd3117e34879b25269a6ae
Finomnis <finomnis@gmail.com>
2019-10-01 17:30:51 +0200

    Minor fix

--- src/location_provider/XContest/XContestAnimation.js
+++ src/location_provider/XContest/XContestAnimation.js
@@ -7,7 +7,7 @@ import { getDistance } from "geolib";
 
 function eqSet(as, bs) {
   if (as.size !== bs.size) return false;
-  for (var a of as) if (!bs.has(a)) return false;
+  for (const a of as) if (!bs.has(a)) return false;
   return true;
 }
 
