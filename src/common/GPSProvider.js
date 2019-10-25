cac0ff6eafe253812b78a9cd4422442fd7cd4fc8
Finomnis <finomnis@gmail.com>
2019-10-25 17:16:24 +0200

    Added GPSProvider

---
+++ src/common/GPSProvider.js
@@ -0,0 +1,73 @@
+import { getSetting, Settings } from "./PersistentState/Settings";
+
+class GPSProvider {
+  constructor() {
+    this._callbacks = [];
+    this._watchId = null;
+    this._gpsData = null;
+
+    let gpsEnabledSetting = getSetting(Settings.GPS_ENABLED);
+    gpsEnabledSetting.registerCallback(this._updateGPSState);
+    this._updateGPSState();
+  }
+
+  _updateGPSState = () => {
+    let gpssettingEnabled = getSetting(Settings.GPS_ENABLED).getValue();
+    let callbackRegistered = this._callbacks.length > 0;
+
+    let enableGps = gpssettingEnabled && callbackRegistered;
+
+    if (this._watchId === null && enableGps) {
+      this._watchId = navigator.geolocation.watchPosition(
+        this._onNewGPSData,
+        this._onGPSError,
+        { enableHighAccuracy: true }
+      );
+    }
+
+    if (this._watchId !== null && !enableGps) {
+      navigator.geolocation.clearWatch(this._watchId);
+      this._watchId = null;
+      this._setGPSData(null);
+    }
+
+    console.log("UPDATE_GPS_STATE", enableGps);
+  };
+
+  _setGPSData = data => {
+    console.log("GPS DATA:", data);
+    this._gpsData = data;
+    for (const cb of this._callbacks) {
+      cb(data);
+    }
+  };
+
+  _onNewGPSData = data => {
+    this._setGPSData(data);
+  };
+  _onGPSError = () => {
+    this._setGPSData(null);
+  };
+
+  registerCallback = cb => {
+    if (!this._callbacks.includes(cb)) this._callbacks.push(cb);
+    this._updateGPSState();
+  };
+
+  unregisterCallback = cb => {
+    const index = this._callbacks.indexOf(cb);
+    if (index >= 0) {
+      this._callbacks.splice(index, 1);
+    }
+    this._updateGPSState();
+  };
+}
+
+// Singleton stuff
+let _instance = null;
+export const getGPSProvider = () => {
+  if (!_instance) {
+    _instance = new GPSProvider();
+  }
+  return _instance;
+};
--- src/pages/PilotList/AnimatedPilotListEntry.jsx
+++ src/pages/PilotList/AnimatedPilotListEntry.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import { Box } from "@material-ui/core";
 import { getXContestInterface } from "../../location_provider/XContest/XContestInterface";
+import { getGPSProvider } from "../../common/GPSProvider";
 
 class AnimatedPilotListEntry extends Component {
   constructor(props) {
     super(props);
     this.pilotInfo = null;
+    this.gpsData = null;
     this.state = {
       online: false
     };
   }
+
+  onNewGPSDataReceived = gpsData => {
+    this.gpsData = gpsData;
+
+    // TODO replace with a more efficient way
+    this.forceUpdate();
+  };
 
   onNewDataReceived = pilotData => {
     if (!(this.props.pilotId in pilotData)) {
@@ -27,9 +36,11 @@ class AnimatedPilotListEntry extends Component {
 
   componentDidMount() {
     getXContestInterface().animation.registerCallback(this.onNewDataReceived);
+    getGPSProvider().registerCallback(this.onNewGPSDataReceived);
   }
   componentWillUnmount() {
     getXContestInterface().animation.unregisterCallback(this.onNewDataReceived);
+    getGPSProvider().unregisterCallback(this.onNewGPSDataReceived);
   }
 
   render() {
@@ -59,9 +70,22 @@ class AnimatedPilotListEntry extends Component {
         );
       }
     }
+
     return (
       <Box margin="10px" onClick={this.props.removePilot}>
         {this.props.pilotId}
+        <Box>
+          GPS:{" "}
+          {this.gpsData === null
+            ? "NULL"
+            : this.gpsData.coords.accuracy.toString()}
+        </Box>
+        <Box>
+          GPS time:{" "}
+          {this.gpsData === null
+            ? "NULL"
+            : Math.round(this.gpsData.timestamp / 1000).toString()}
+        </Box>
         <table>
           <tbody>{pilotDebugOutput}</tbody>
         </table>
