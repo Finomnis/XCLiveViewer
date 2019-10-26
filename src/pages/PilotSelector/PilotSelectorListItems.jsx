1c4fba6a785376b474c1dd8e987ac15b40358507
Finomnis <finomnis@gmail.com>
2019-10-26 12:14:21 +0200

    Fixed warning

--- src/pages/PilotSelector/PilotSelectorListItems.jsx
+++ src/pages/PilotSelector/PilotSelectorListItems.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { TableRow, TableCell, Box, Typography } from "@material-ui/core";
 import LastFixState, { LastFixArrow } from "../../util/LastFixState";
 
