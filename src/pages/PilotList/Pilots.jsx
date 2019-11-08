44091d5c5ad17371529d1459ae5355033e2f66a7
Finomnis <finomnis@gmail.com>
2019-11-09 00:12:25 +0100

    rewrote 'Pilots' page as PureComponent

--- src/pages/PilotList/Pilots.jsx
+++ src/pages/PilotList/Pilots.jsx
@@ -1,49 +1,52 @@
 import React from "react";
-import { useState } from "react";
 import { Fab } from "@material-ui/core";
-import { useTheme } from "@material-ui/core/styles";
 import AddIcon from "@material-ui/icons/Add";
 import Box from "@material-ui/core/Box";
 import PilotSelector from "../PilotSelector/PilotSelector";
-import { useChosenPilots } from "../../common/PersistentState/ChosenPilots";
+import {
+  getChosenPilots,
+  addPilots,
+  removePilots
+} from "../../common/PersistentState/ChosenPilots";
 import AnimatedPilotList from "./AnimatedPilotList";
 
-const Pilots = () => {
-  const theme = useTheme();
-  const [pilotSelectorOpen, setPilotSelectorOpen] = useState(false);
+class Pilots extends React.PureComponent {
+  state = { pilotSelectorOpen: false };
 
-  const [pilots, addPilots, removePilots] = useChosenPilots();
+  openPilotSelector = () => {
+    if (this.state.pilotSelectorOpen === false)
+      this.setState({ pilotSelectorOpen: true });
+  };
 
-  // TODO sort pilots
+  closePilotSelector = () => {
+    if (this.state.pilotSelectorOpen === true)
+      this.setState({ pilotSelectorOpen: false });
+  };
 
-  return (
-    <React.Fragment>
-      <AnimatedPilotList
-        pilots={pilots}
-        removePilots={removePilots}
-      ></AnimatedPilotList>
+  render() {
+    const pilots = getChosenPilots();
 
-      <Box
-        position="absolute"
-        bottom={theme.spacing(2)}
-        right={theme.spacing(2)}
-      >
-        <Fab
-          size="small"
-          color="primary"
-          onClick={() => setPilotSelectorOpen(true)}
-        >
-          <AddIcon />
-        </Fab>
-      </Box>
-      <PilotSelector
-        open={pilotSelectorOpen}
-        onClose={() => setPilotSelectorOpen(false)}
-        onAddPilots={addPilots}
-        alreadyAdded={Object.keys(pilots)}
-      />
-    </React.Fragment>
-  );
-};
+    return (
+      <React.Fragment>
+        <AnimatedPilotList
+          pilots={pilots}
+          removePilots={removePilots}
+        ></AnimatedPilotList>
+
+        <Box position="absolute" bottom="16px" right="16px">
+          <Fab size="small" color="primary" onClick={this.openPilotSelector}>
+            <AddIcon />
+          </Fab>
+        </Box>
+        <PilotSelector
+          open={this.state.pilotSelectorOpen}
+          onClose={this.closePilotSelector}
+          onAddPilots={addPilots}
+          alreadyAdded={Object.keys(pilots)}
+        />
+      </React.Fragment>
+    );
+  }
+}
 
 export default Pilots;
