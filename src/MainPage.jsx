828bbaf35af614de61e6d1fe1350867b99014343
Finomnis <finomnis@gmail.com>
2019-11-23 23:50:14 +0100

    Removed third tab, removed tabs all together on desktop and simply put the pages side by side

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
 import SwipeableViews from "react-swipeable-views";
-import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  useMediaQuery,
+  useTheme
+} from "@material-ui/core";
 
-import LocationOnIcon from "@material-ui/icons/LocationOn";
 import PeopleIcon from "@material-ui/icons/People";
 import MapIcon from "@material-ui/icons/Map";
 
@@ -18,6 +22,8 @@ import TitleBar from "./pages/TitleBar";
 const MainPage = () => {
   const [tabId, setTabId] = React.useState(1);
   const [windowHeight, setWindowHeight] = React.useState(0);
+  const theme = useTheme();
+  const isSmartphone = useMediaQuery(theme.breakpoints.down("sm"));
 
   // Workaround for mobile screens reporting an incorrect window height
   const updateWindowHeight = () => {
@@ -32,11 +38,8 @@ const MainPage = () => {
   // Callbacks for changing the current page
   const handleNavigationButton = (_event, newValue) => setTabId(newValue);
 
-  return (
-    <Box height={windowHeight} display="flex" flexDirection="column">
-      <Box zIndex={100}>
-        <TitleBar></TitleBar>
-      </Box>
+  const content = isSmartphone ? (
+    <React.Fragment>
       <Box flexGrow={1} clone>
         <SwipeableViews disabled index={tabId}>
           <Box width="100%" height="100%" position="relative">
@@ -44,9 +47,6 @@ const MainPage = () => {
           </Box>
           <Box width="100%" height="100%" position="relative">
             <Pilots></Pilots>
-          </Box>
-          <Box width="100%" height="100%" position="relative">
-            Item Three
           </Box>
         </SwipeableViews>
       </Box>
@@ -58,9 +58,27 @@ const MainPage = () => {
         >
           <BottomNavigationAction label="Map" icon={<MapIcon />} />
           <BottomNavigationAction label="Pilots" icon={<PeopleIcon />} />
-          <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
         </BottomNavigation>
       </Box>
+    </React.Fragment>
+  ) : (
+    <Box width="100%" flexGrow={1} display="flex">
+      <Box flexGrow={1} height="100%">
+        <LiveMap></LiveMap>
+      </Box>
+      <Box width="450px" height="100%" zIndex={50} boxShadow={3}>
+        <Pilots></Pilots>
+      </Box>
+    </Box>
+  );
+
+  return (
+    <Box height={windowHeight} display="flex" flexDirection="column">
+      <Box zIndex={100}>
+        <TitleBar></TitleBar>
+      </Box>
+
+      {content}
     </Box>
   );
 };
