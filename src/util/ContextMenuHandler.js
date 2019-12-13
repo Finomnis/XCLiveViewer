fec0b8579c5affba592f3432c69cf53bb8f1f128
Finomnis <finomnis@gmail.com>
2019-12-13 22:00:32 +0100

    Reduced ContextMenuHandler time for iOS, to work around the problem where apple opens a select menu instead

--- src/util/ContextMenuHandler.js
+++ src/util/ContextMenuHandler.js
@@ -1,4 +1,9 @@
-const longPressDuration = 610;
+const longPressDuration =
+  navigator.platform.indexOf("iPhone") !== -1 ||
+  navigator.platform.indexOf("iPad") !== -1 ||
+  navigator.platform.indexOf("iPod") !== -1
+    ? 300
+    : 1000;
 
 export default class ContextMenuHandler {
   constructor(callback) {
@@ -8,6 +13,7 @@ export default class ContextMenuHandler {
   }
 
   onTouchStart = e => {
+    console.log(this.toString(), "onTouchStart");
     this.contextMenuPossible = true;
 
     const touch = e.touches[0];
@@ -15,24 +21,29 @@ export default class ContextMenuHandler {
     this.longPressCountdown = setTimeout(() => {
       this.contextMenuPossible = false;
       this.callback(touch);
+      console.log(this.toString(), "CONTEXT_MENU");
     }, longPressDuration);
   };
 
   onTouchMove = e => {
+    console.log(this.toString(), "onTouchMove");
     clearTimeout(this.longPressCountdown);
   };
 
   onTouchCancel = e => {
+    console.log(this.toString(), "onTouchCancel");
     this.contextMenuPossible = false;
     clearTimeout(this.longPressCountdown);
   };
 
   onTouchEnd = e => {
+    console.log(this.toString(), "onTouchEnd");
     this.contextMenuPossible = false;
     clearTimeout(this.longPressCountdown);
   };
 
   onContextMenu = e => {
+    console.log(this.toString(), "onContextMenu");
     this.contextMenuPossible = false;
 
     clearTimeout(this.longPressCountdown);
